542ede4cb8d63ae372b350cf554aace82d2fd711
Pavel Cisar <pavel.cisar@coex.cz>
2017-03-03 11:36:21 +0100

    Added pycharm files to gitignore Upgrade for TinyMCE4

--- elfinder/views_tinymce.py
+++ elfinder/views_tinymce.py
@@ -16,8 +16,5 @@ def tinymce_filebrowser_dialog_view(request):
     """
     View for rendering elfinder with TinyMCE popup bindings.
     """
-    tinymce_popup_js = settings.ELFINDER_TINYMCE_PATH_TO_POPUP_JS
-    if tinymce_popup_js is None:
-        raise ImproperlyConfigured("ELFINDER_TINYMCE_PATH_TO_POPUP_JS must be specified in settings")
 
-    return render(request, "elfinder_tinymce_filebrowser_dialog.html", {"tinymce_popup_js": tinymce_popup_js})
+    return render(request, "elfinder_tinymce_filebrowser_dialog.html")
